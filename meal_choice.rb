89abf360cb0772a302d5e04da6de46d03b408314
Yuta Katsuyama <ykatsuyama@id.iit.edu>
2019-03-28 22:32:41 +0000

    Automatically backed up by Learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,5 +1,7 @@
 # Define a method meal_choice that returns the meal_choice that was
 # passed into it and defaults to meat.
 
-def meal_choice do
+def meal_choice(meal="meat") do
+  puts meal
+end
   
